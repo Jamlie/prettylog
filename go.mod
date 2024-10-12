8f065ff002e0551f0303e69d13d7a23b36672a73
Omar <jamlee977@outlook.com>
2024-10-12 21:09:51 +0300

    pretty logger

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Jamlie/prettylog
+
+go 1.23.0
+
+require github.com/Jamlie/colors v0.0.0-20240902103838-d0a86a3f0c49 // indirect
---
+++ main.go
@@ -0,0 +1,148 @@
+package prettylog
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"sync"
+
+	"github.com/Jamlie/colors"
+)
+
+const (
+	timeFormat = "[15:04:05]"
+)
+
+var (
+	messageColor = colors.New(colors.WhiteFg)
+	attrsColor   = colors.NewCustomId(245)
+
+	infoColor    = colors.NewCustomId(40)
+	warningColor = colors.NewCustomId(202)
+	errorColor   = colors.NewCustomId(196)
+	debugColor   = colors.NewCustomId(57)
+
+	timeColor     = colors.NewCustomId(188)
+	datetimeColor = colors.NewCustomId(178)
+	fileColor     = colors.NewCustomId(12)
+)
+
+type Handler struct {
+	h slog.Handler
+	b *bytes.Buffer
+	m *sync.Mutex
+}
+
+func NewHandler(opts *slog.HandlerOptions) *Handler {
+	if opts == nil {
+		opts = &slog.HandlerOptions{}
+	}
+
+	b := bytes.NewBuffer(nil)
+	return &Handler{
+		h: slog.NewJSONHandler(b, &slog.HandlerOptions{
+			AddSource:   opts.AddSource,
+			Level:       opts.Level,
+			ReplaceAttr: suppressDefaults(opts.ReplaceAttr),
+		}),
+		b: b,
+		m: &sync.Mutex{},
+	}
+}
+
+func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.h.Enabled(ctx, level)
+}
+
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &Handler{
+		h: h.h.WithAttrs(attrs),
+		b: h.b,
+		m: h.m,
+	}
+}
+
+func (h *Handler) WithGroup(name string) slog.Handler {
+	return &Handler{
+		h: h.h.WithGroup(name),
+		b: h.b,
+		m: h.m,
+	}
+}
+
+func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
+	level := r.Level.String() + ":"
+
+	switch r.Level {
+	case slog.LevelDebug:
+		level = debugColor.String(level)
+	case slog.LevelInfo:
+		level = infoColor.String(level)
+	case slog.LevelWarn:
+		level = warningColor.String(level)
+	case slog.LevelError:
+		level = errorColor.String(level)
+	}
+
+	attrs, err := h.computeAttrs(ctx, r)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := json.MarshalIndent(attrs, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error when marshaling attrs: %w", err)
+	}
+
+	fmt.Println(
+		datetimeColor.String(r.Time.Format(timeFormat)),
+		level,
+		messageColor.String(r.Message),
+		attrsColor.String(string(bytes)),
+	)
+
+	return nil
+}
+
+func (h *Handler) computeAttrs(
+	ctx context.Context,
+	r slog.Record,
+) (map[string]any, error) {
+	h.m.Lock()
+	defer func() {
+		h.b.Reset()
+		h.m.Unlock()
+	}()
+
+	if err := h.h.Handle(ctx, r); err != nil {
+		return nil, fmt.Errorf("error when calling inner handler's Handle: %w", err)
+	}
+
+	var attrs map[string]any
+	err := json.Unmarshal(h.b.Bytes(), &attrs)
+	if err != nil {
+		return nil, fmt.Errorf("error when unmarshaling inner handler's Handle result: %w", err)
+	}
+
+	return attrs, nil
+}
+
+func suppressDefaults(
+	next func([]string, slog.Attr) slog.Attr,
+) func([]string, slog.Attr) slog.Attr {
+	return func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey ||
+			a.Key == slog.LevelKey ||
+			a.Key == slog.MessageKey {
+			return slog.Attr{}
+		}
+
+		if next == nil {
+			return a
+		}
+
+		return next(groups, a)
+	}
+}
